e14bf9c135f1e7ddb86bdedc7b27cc48fc49344c
Sergey Batukov <sashachepuha@gmail.com>
2021-12-23 14:40:22 +0000

    update

--- update.js
+++ update.js
@@ -2,10 +2,26 @@ const key = process.env.FIGMA_KEY
 const id = process.env.FILE_ID
 
 const axios = require('axios');
+const fs = require('fs')
 
 axios.get(`https://api.figma.com/v1/files/${id}?geometry=paths`, {
   headers: {
     "X-FIGMA-TOKEN": key
   }
 })
-  .then(res => console.log(JSON.stringify(res.data, null, ' ')))
+  .then(res => write(res.data))
+
+
+function write(data) {
+  const newData = data.document.children[0].children
+  const result = {}
+  newData.forEach(icon => {
+    result[icon.name] = {}
+    icon.children.forEach(mode => {
+      result[icon.name][mode.name] = mode.fillGeometry[0].path
+    })
+  });
+  fs.writeFileSync('./iconica/assets/lib.json', JSON.stringify(result, null, '  '), (err) => {
+    console.log(err)
+  })
+}
