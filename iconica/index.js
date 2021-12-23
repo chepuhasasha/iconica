7123b4979df17863006bef057be08a12d39fdc9d
Sergey Batukov <sashachepuha@gmail.com>
2021-12-23 13:08:40 +0000

    update

--- iconica/index.js
+++ iconica/index.js
@@ -4,6 +4,7 @@ export default function icon(name) {
   const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
   const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
   path.setAttributeNS(null, 'd', lib[name]);
+  path.setAttributeNS(null, 'fill', 'red');
   svg.appendChild(path)
   return svg
 }
