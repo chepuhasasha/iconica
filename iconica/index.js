0ec84a7732c9f5d2b64d96fedc74dbbffbd0952f
Sergey Batukov <sashachepuha@gmail.com>
2021-12-24 07:53:28 +0000

    update

--- iconica/index.js
+++ iconica/index.js
@@ -2,9 +2,10 @@ import lib from './assets/lib.json'
 
 export default function icon(name) {
   const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-  svg.setAttributeNS(null, 'width', 20);
-  svg.setAttributeNS(null, 'height', 20);
   const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+  svg.setAttributeNS(null, 'viewBox', '0 0 20 20');
+  svg.setAttributeNS(null, 'width', 100);
+  svg.setAttributeNS(null, 'height', 100);
   path.setAttributeNS(null, 'd', lib[name].default);
   svg.appendChild(path)
   return svg
